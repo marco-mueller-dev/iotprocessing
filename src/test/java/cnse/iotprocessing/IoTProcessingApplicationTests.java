c02d3b4dd4f4d9eaf9ec76ad381a0835317489ad
marco <mamu@stud.hs-kl.de>
2025-12-11 12:49:21 +0100

    Initial commit: imported from Spring Initializr

---
+++ src/test/java/cnse/iotprocessing/IoTProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package cnse.iotprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
