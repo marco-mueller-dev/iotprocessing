d2b8ae658f1f13aa0cf93bbdf45c2fb216d51f9a
Lars <lars.greuzinger@gmail.com>
2026-01-20 00:03:48 +0100

    #6-DB-Begrenzung

--- iot-data-generator/src/main/java/com/example/iotdatagenerator/SensorGenerator.java
+++ iot-data-generator/src/main/java/com/example/iotdatagenerator/SensorGenerator.java
@@ -67,8 +67,18 @@ public class SensorGenerator {
                 Instant.now()
         );
 
+
+        // Maximal 10 Einträge halten
+        long count = repository.count();
+        if (count >= 10) {
+            SensorData oldest = repository.findAllByOrderByTimestampAsc().get(0);
+            repository.delete(oldest);
+        }
+
         repository.save(data);
     }
+
+
     private double randTemp () {
         int a = ThreadLocalRandom.current().nextInt(0, 3);
         if (a == 2) {
