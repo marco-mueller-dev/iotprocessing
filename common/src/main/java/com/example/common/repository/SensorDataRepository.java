9d02277188abfc2da6efda02930fa0869b18792a
marco <mamu@stud.hs-kl.de>
2026-01-22 16:57:03 +0100

    #9-cleanup Dependencies aufgeräumt

--- common/src/main/java/com/example/common/repository/SensorDataRepository.java
+++ common/src/main/java/com/example/common/repository/SensorDataRepository.java
@@ -3,8 +3,6 @@ package com.example.common.repository;
 import com.example.common.entity.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
 
 }
