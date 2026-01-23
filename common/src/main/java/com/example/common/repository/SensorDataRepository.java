8c0ddeaef699f813d2042cd409626388bf041a13
Lars <lars.greuzinger@gmail.com>
2026-01-23 20:26:45 +0100

    #11, List import

--- common/src/main/java/com/example/common/repository/SensorDataRepository.java
+++ common/src/main/java/com/example/common/repository/SensorDataRepository.java
@@ -4,6 +4,8 @@ import com.example.common.entity.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
 
     @Query("""
